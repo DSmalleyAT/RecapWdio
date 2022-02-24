bfe253f07f3087ca357f6d4b3b1a309d64d9b918
MacMini17 <mcm-mac-17@MacMini17s-Mac-mini.local>
2022-02-24 09:47:10 -0800

    Cleanup

--- test/specs/dynamic-loading.spec.js
+++ test/specs/dynamic-loading.spec.js
@@ -3,19 +3,12 @@ const DynamicLoadingPage = require('../pageobjects/dynamic-loading.page');
 
 
 describe('My Dynamic Loading Page', () => {
-    describe('My INITIAL Dynamic Loading Page', () => {
-        it('should verify that start button exists', async () => {
-            await DynamicLoadingPage.open();
-            await expect(DynamicLoadingPage.startButton).toBeExisting();
-        
-        });
-    });
-    describe('My WORKING Dynamic Loading Page', () => {
-        it ('should click the start button', async () => {
+    describe('My Start Button...', () => {
+        it ('...should be clickable', async () => {
             await DynamicLoadingPage.open();
             await (DynamicLoadingPage.startButton).click();
         });
-        it ('should load a Hello World Message', async () => {
+        it ('...should load a Hello World Message on click', async () => {
             await (DynamicLoadingPage.HelloWorld).waitForDisplayed ({timeout: 10000});
         });
     });
